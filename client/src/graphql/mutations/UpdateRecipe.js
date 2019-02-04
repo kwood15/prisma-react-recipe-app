39ab4d9ddef4d80ffc7693ec8cf9466050a32b20
Kay Wood <kwood15@users.noreply.github.com>
2019-02-04 18:39:06 +0000

    Add queries/mutations

---
+++ client/src/graphql/mutations/UpdateRecipe.js
@@ -0,0 +1,21 @@
+import { gql } from 'apollo-boost';
+
+export default gql`mutation UpdateRecipe(
+    $id: ID!
+    $directions: String!
+    $title: String!
+    $ingredients: String!
+    $published: Boolean
+  ) {
+    updateRecipe(
+      where: { id: $id }
+      data: {
+        directions: $directions
+        title: $title
+        ingredients: $ingredients
+        published: $published
+      }
+    ) {
+      id
+    }
+  }`;
--- client/src/graphql/queries/GetSingleRecipe.js
+++ client/src/graphql/queries/GetSingleRecipe.js
@@ -1,12 +1,12 @@
 import { gql } from 'apollo-boost';
 
-export default gql `query GetAllPublishedRecipes {
-    recipes(where: { published: true }) {
-      id
-      createdAt
-      title
-      ingredients
-      directions
-      published
-    }
-  }`;
+export default gql`query GetSingleRecipe($recipeId: ID!) {
+  recipe(where: { id: $recipeId }) {
+    id
+    createdAt
+    title
+    directions
+    ingredients
+    published
+  }
+}`;
